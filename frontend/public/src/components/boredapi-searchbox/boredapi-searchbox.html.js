0498eff93e846787adfaff1a92127bd83cfc8fb3
jstpierre2906 <jstpierre2906@outlook.com>
2024-05-17 21:22:35 -0400

    html

---
+++ frontend/public/src/components/boredapi-searchbox/boredapi-searchbox.html.js
@@ -0,0 +1,37 @@
+export const html = ({ textContent, types, participants, durations, orderbys, iMaxResults }) => /*html*/ `
+<link rel="stylesheet" href="/src/components/boredapi-searchbox/boredapi-searchbox.css" />
+<div>
+  <h2>${textContent}</h2>
+  <form>
+    <div>
+      <label for="activityId">ID</label>
+      <input type="number" id="activityId" maxlength="7" placeholder="0000000" />
+    </div>
+    <div>
+      <label for="description">Description</label>
+      <input type="text" id="description" placeholder="Some description" />
+    </div>
+    <div>
+      <label for="type">Type</label>
+      <select id="type">${types.join("")}</select>
+    </div>
+    <div>
+      <label for="participants">Participants</label>
+      <select id="participants">${participants.join("")}</select>
+    </div>
+    <div>
+      <label for="duration">Duration</label>
+      <select id="duration">${durations.join("")}</select>
+    </div>
+    <div>
+      <label for="orderby">Order by</label>
+      <select id="orderby">${orderbys.join("")}</select>
+    </div>
+    <div>
+      <input id="maxResults" type="hidden" value="${iMaxResults}" />
+      <input id="resetBtn" type="reset" value="Reset" />
+      <input id="searchBtn" type="button" value="Search" />
+    </div>
+  </form>
+</div>
+`;
--- frontend/public/src/components/boredapi-searchbox/boredapi-searchbox.js
+++ frontend/public/src/components/boredapi-searchbox/boredapi-searchbox.js
@@ -1,4 +1,5 @@
 import { Component } from "@components/component.js";
+import { html } from "./boredapi-searchbox.html.js";
 import {
   durationsModel,
   orderbyModel,
@@ -39,43 +40,14 @@ export class BoredAPISearchBox extends Component {
           )
         );
         const template = document.createElement("template");
-        template.innerHTML = /*html*/ `
-          <link rel="stylesheet" href="/src/components/boredapi-searchbox/boredapi-searchbox.css" />
-          <div>
-            <h2>${this.textContent}</h2>
-            <form>
-              <div>
-                <label for="activityId">ID</label>
-                <input type="number" id="activityId" maxlength="7" placeholder="0000000" />
-              </div>
-              <div>
-                <label for="description">Description</label>
-                <input type="text" id="description" placeholder="Some description" />
-              </div>
-              <div>
-                <label for="type">Type</label>
-                <select id="type">${types.join("")}</select>
-              </div>
-              <div>
-                <label for="participants">Participants</label>
-                <select id="participants">${participants.join("")}</select>
-              </div>
-              <div>
-                <label for="duration">Duration</label>
-                <select id="duration">${durations.join("")}</select>
-              </div>
-              <div>
-                <label for="orderby">Order by</label>
-                <select id="orderby">${orderbys.join("")}</select>
-              </div>
-              <div>
-                <input id="maxResults" type="hidden" value="${this.iMaxResults}" />
-                <input id="resetBtn" type="reset" value="Reset" />
-                <input id="searchBtn" type="button" value="Search" />
-              </div>
-            </form>
-          </div>
-        `;
+        template.innerHTML = html({
+          textContent: this.textContent,
+          types: types,
+          participants: participants,
+          durations: durations,
+          orderbys: orderbys,
+          iMaxResults: this.iMaxResults,
+        });
         return template.content.cloneNode(true);
       })()
     );
