ddd948d52a6d70536d912d2e522697e0748ee3a7
jstpierre2906 <jstpierre2906@outlook.com>
2024-05-21 16:19:09 -0400

    stylin'

--- frontend/public/src/components/boredapi-searchresults/templates/boredapi-searchresult.html.js
+++ frontend/public/src/components/boredapi-searchresults/templates/boredapi-searchresult.html.js
@@ -1,20 +1,23 @@
 const htmlLink = ({ link }) => /*html*/ `
-  <ul class="thirdRow"><li class="link">Link: <a href="${link}" target="_blank">${link}</a></li></ul>
+  <ul class="thirdRow">
+    <li class="left">
+      <span class="label">Link:</span>
+      <a href="${link}" target="_blank">${link}</a>
+    </li>
+  </ul>
 `;
 
 export const htmlResult = ({ activity, key, type, participants, duration, link }) => /*html*/ `
 <div class="result">
   <h3>${activity}</h3>
-  <div>
-    <ul class="firstRow">
-      <li class="id">ID: ${key}</li>
-      <li class="type">Type: ${type}</li>
-    </ul>
-    <ul class="secondRow">
-      <li class="participants">Participants: ${participants}</li>
-      <li class="duration">Duration: ${duration}</li>
-    </ul>
-    ${link && htmlLink({ link: link })}
-  </div>
+  <ul class="firstRow">
+    <li class="left"><span class="label">ID: </span>${key}</li>
+    <li class="right"><span class="label">Type: </span>${type}</li>
+  </ul>
+  <ul class="secondRow">
+    <li class="left"><span class="label">Participants: </span>${participants}</li>
+    <li class="right"><span class="label">Duration: </span>${duration}</li>
+  </ul>
+  ${link && htmlLink({ link: link })}
 </div>
 `;
--- frontend/public/src/components/boredapi-searchresults/templates/boredapi-searchresults.html.js
+++ frontend/public/src/components/boredapi-searchresults/templates/boredapi-searchresults.html.js
@@ -1,7 +1,7 @@
 export const html = ({ results, emptyResultset }) => /*html*/ `
 <link rel="stylesheet" href="/src/components/boredapi-searchresults/styles/boredapi-searchresults.css" />
 <div>
-  <h2>Search results</h2>
+  <h2 id="resultsDescription">Search results</h2>
   <div id="results">${results.join("")}</div>
   <div id="empty-resultset">${emptyResultset}</div>
 </div>
