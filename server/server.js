14fdc84d4e105c860c0ed7679efc5dfed090dc23
jstpierre2906 <jstpierre2906@outlook.com>
2024-06-07 21:32:16 -0400

    better server loging

--- server/server.js
+++ server/server.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const path = require("path");
 
-const host = "127.0.0.1";
-const port = 9001;
+const HOST = "127.0.0.1";
+const PORT = 9001;
+const ROOT_DIR = path.join(__dirname, "../public");
+const LOG_JS_FILES = false;
 
 const app = express();
 
 // STATIC FILES ---------------------------------------------------------------
-app.use("/", express.static(path.join(__dirname, "../public")));
-app.listen(port, host, () => console.log(`Server started at ${host}:${port}, ${new Date()}`));
+app.use((request, response, next) => {
+  const shouldBeLogged = () => {
+    return (
+      request.url === "/" ||
+      request.url.endsWith(".html") ||
+      (LOG_JS_FILES ? request.url.endsWith(".js") : false)
+    );
+  };
+  if (shouldBeLogged()) {
+    const str = "{date} - {method} {url}";
+    const log = str
+      .replace("{date}", new Date().toISOString())
+      .replace("{method}", request.method)
+      .replace("{url}", request.url);
+    console.log(log);
+  }
+  next();
+});
+app.use("/", express.static(ROOT_DIR));
+app.listen(PORT, HOST, () => {
+  const str = "Server started at {host}:{port} - {date}";
+  const log = str
+    .replace("{host}", HOST)
+    .replace("{port}", PORT)
+    .replace("{date}", new Date().toISOString());
+  console.log(log);
+});
