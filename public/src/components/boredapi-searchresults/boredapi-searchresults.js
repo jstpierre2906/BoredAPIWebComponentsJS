14c332097c1639a0a8d297ebb97515ab26e65647
jstpierre2906 <jstpierre2906@outlook.com>
2024-07-14 12:46:35 -0400

    starting with front-end api calls

--- public/src/components/boredapi-searchresults/boredapi-searchresults.js
+++ public/src/components/boredapi-searchresults/boredapi-searchresults.js
@@ -32,67 +32,38 @@ export class BoredAPISearchResults extends Component {
 
   /** @param {{ searchObj: Object }} */
   #searchHandler({ searchObj }) {
-    console.log(searchObj);
-
-    this.#fetchResults(searchObj)
-      .then((results) => this.#setResults(results))
-      .catch((error) => this.#unsetResults());
-  }
-
-  /**
-   * Mock back-end call; will be removed when back-end is up and running.
-   *
-   * @param {Object} searchQuery
-   * @returns {string[]}
-   */
-  #fetchResults(searchQuery) {
-    const queryString = Object.keys(searchQuery)
-      .filter((key) => searchQuery[key] !== "")
-      .map((key) => `${key}=${searchQuery[key]}`)
-      .join("&");
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        fetch(`/mock-data/activities.json?${queryString}`, {
-          method: "GET",
-          mode: "cors",
-          headers: { "Content-Type": "application/json" },
-        })
+    /** @returns {Promise<string>} */
+    (() => {
+      const apiURL = "http://127.0.0.1:9000/api/v0.9.4";
+      let route = "";
+      console.log(searchObj);
+      const isFindAll = () => {
+        return Object.keys(searchObj)
+          .filter((k) => k !== "maxResults")
+          .every((k) => searchObj[k] === "");
+      };
+      if (isFindAll()) {
+        route = `${apiURL}/activities`;
+      } else {
+        return Promise.reject(new Error("Front-end API call not implemented yet"));
+      }
+      return new Promise((resolve, reject) => {
+        fetch(route, { method: "GET" })
           .then((response) => {
             if (response.status !== 200) {
-              reject(`Error: Response status ${response.status}`);
+              reject(new Error(`Error: Response status ${response.status}`));
             }
             return response.json();
           })
-          .then((data) => {
-            const filteredData = data
-              .filter((d) =>
-                searchQuery.activityId ? d.key.includes(searchQuery.activityId.toString()) : true
-              )
-              .filter((d) =>
-                searchQuery.description
-                  ? d.activity.toLowerCase().includes(searchQuery.description.toLowerCase())
-                  : true
-              )
-              .filter((d) => (searchQuery.type ? d.type === searchQuery.type : true))
-              .filter((d) => {
-                if (!searchQuery.participants) {
-                  return true;
-                }
-                if (searchQuery.participants.endsWith("+")) {
-                  const participants = +searchQuery.participants.replace("+", "");
-                  return d.participants >= participants;
-                }
-                return d.participants === +searchQuery.participants;
-              })
-              .filter((d) => (searchQuery.duration ? d.duration === searchQuery.duration : true));
-
-            // TODO Sort to be done Java-side
-            filteredData.splice(+searchQuery.maxResults);
-            resolve(filteredData);
-          })
-          .catch((err) => reject(`Error: Problem with JSON formatting - ${err}`));
-      }, 100);
-    });
+          .then((data) => resolve(data))
+          .catch((error) => reject(error));
+      });
+    })()
+      .then((results) => this.#setResults(results))
+      .catch((error) => {
+        console.log(error);
+        this.#unsetResults();
+      });
   }
 
   #unsetResults() {
